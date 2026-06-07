2b5874a2f472815c33b12d9874ec847002586be4
Anisur Rahman <anisoncloud@gmail.com>
2026-06-07 23:16:56 +0600

    IUnitOfWork

---
+++ IServices/IUnitOfWrk.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication1.Contract;
+using WebApplication1.Models;
+
+namespace WebApplication1.IServices
+{
+    public interface IUnitOfWrk : IDisposable
+    {
+        IRepository<Category> Categories { get; }
+        IRepository<Product> Products { get; }
+        Task<int> CommitAsync();
+        Task RollBackAsync();
+    }
+}
